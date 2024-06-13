99e41ebc206da512a4d8f8bf1dd1f1eeaf6db503
Abdelrahman Shaheen <abshahin8@outlook.com>
2024-06-13 11:32:01 +0300

    p

---
+++ keystatic.config.ts
@@ -0,0 +1,49 @@
+import {config, fields, collection} from '@keystatic/core';
+
+export default config({
+    storage: {
+        kind: 'github',
+        repo: 'aashahin/myblog',
+        branchPrefix: 'main',
+        pathPrefix: 'content',
+    },
+    collections: {
+        posts: collection({
+            label: 'Posts',
+            slugField: 'title',
+            path: 'src/content/posts/*',
+            format: {contentField: 'content'},
+            schema: {
+                title: fields.slug({name: {label: 'Title'}}),
+                description: fields.text({label: 'Description'}),
+                content: fields.markdoc({
+                    label: 'Content',
+                    options: {
+                        image: {
+                            directory: 'src/assets/images/posts',
+                            publicPath: '@assets/images/posts/'
+
+                        }
+                    }
+                }),
+                date: fields.date({label: 'Date'}),
+                tags: fields.array(
+                    fields.relationship({
+                        label: 'Tags',
+                        collection: 'tags',
+                        description: 'Tags for this post',
+                    })
+                )
+            },
+        }),
+        tags: collection({
+            label: 'Tags',
+            slugField: 'name',
+            path: 'src/content/tags/*',
+            format: {contentField: 'content'},
+            schema: {
+                name: fields.slug({name: {label: 'Name'}}),
+            },
+        }),
+    },
+});
