e432ef592484edd5ea95e34648e45835dc03ab12
Abdelrahman Shaheen <abshahin8@outlook.com>
2024-06-14 10:35:40 +0300

    Added sitemap, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from '@keystatic/astro';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), markdoc(), keystatic()],
+  site: 'https://abdelrahman.co',
+  integrations: [tailwind(), react(), markdoc(), keystatic(), sitemap()],
   output: 'hybrid',
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     }
   })
 });
